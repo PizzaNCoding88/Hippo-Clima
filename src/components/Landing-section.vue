98a01ea5313dae95e5bfed0a556a5e516a93924d
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-02-03 22:58:41 +0000

    sidebar

--- src/components/Landing-section.vue
+++ src/components/Landing-section.vue
@@ -4,10 +4,16 @@
         <div class="logo-container">
             <img src="../images/logo.png" alt="logo" />
         </div>
-        <div class="burger-button">
+        <div class="burger-button" v-on:click="sidebarOpen" >
+            <span :class="{ openTop: isOpen }"></span>
             <span></span>
-            <span></span>
-            <span></span>
+            <span :class="{ openBottom: isOpen }"></span>
+        </div>
+        <div class="sidebar" :class="{ open: isOpen }">
+            <ul>
+                <li class="z-50"><a href="/">Servizi</a></li>
+                <li>Contatti</li>
+            </ul>
         </div>
     </header>
     <main class="">
@@ -16,7 +22,18 @@
 </template>
 
 <script>
-export default{}
+export default{
+    data(){
+        return{
+            isOpen: false
+        }
+    },
+    methods: {
+        sidebarOpen(){
+            this.isOpen = !this.isOpen
+        }
+    },
+}
 </script>
 
 <style scoped>
@@ -49,4 +66,22 @@ img{
 span{
     @apply inline-block bg-orange h-[3px]
 }
+
+.openTop,
+.openBottom{
+    @apply !w-6 duration-500 transition-all ease-in-out
+}
+
+.sidebar{ 
+    @apply absolute left-0 top-0 w-full h-0 -z-10 text-orange flex justify-center items-end flex-row overflow-y-hidden bg-slate-300 rounded-b-xl
+}
+
+.sidebar ul{
+    @apply z-50 flex items-center justify-center gap-[50%] text-xl font-serif pb-4
+}
+
+.open{
+    @apply h-40 transition-all duration-500 opacity-40
+}
+
 </style>
