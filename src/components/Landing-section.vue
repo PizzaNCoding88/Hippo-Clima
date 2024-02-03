aa55115e35824fc7f9f50e50dff1f2a678fa7f1f
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-02-03 23:12:30 +0000

    sidebar 1.5

--- src/components/Landing-section.vue
+++ src/components/Landing-section.vue
@@ -2,16 +2,16 @@
 
     <header>
         <div class="logo-container">
-            <img src="../images/logo.png" alt="logo" />
+            <a href="/"><img src="../images/logo.png" alt="logo" /></a>
         </div>
-        <div class="burger-button" v-on:click="sidebarOpen" >
+        <button class="burger-button" v-on:click="sidebarOpen" >
             <span :class="{ openTop: isOpen }"></span>
             <span></span>
             <span :class="{ openBottom: isOpen }"></span>
-        </div>
+        </button>
         <div class="sidebar" :class="{ open: isOpen }">
             <ul>
-                <li class="z-50"><a href="/">Servizi</a></li>
+                <li class=""><a href="/">Servizi</a></li>
                 <li>Contatti</li>
             </ul>
         </div>
@@ -41,7 +41,7 @@ header{
     @apply w-full flex flex-row justify-between items-center p-8
 }
 .logo-container{
-    @apply w-10 h-10
+    @apply w-10 h-10 z-20
 }
 
 img{
@@ -49,7 +49,7 @@ img{
 }
 
 .burger-button{
-    @apply w-8 h-8 flex flex-col justify-center items-end gap-1
+    @apply w-8 h-8 flex flex-col justify-center items-end gap-1 z-20
 }
 
 .burger-button :first-child{
@@ -73,15 +73,19 @@ span{
 }
 
 .sidebar{ 
-    @apply absolute left-0 top-0 w-full h-0 -z-10 text-orange flex justify-center items-end flex-row overflow-y-hidden bg-slate-300 rounded-b-xl
+    @apply absolute left-0 top-0 w-full h-0 text-orange flex justify-center items-end flex-row overflow-y-hidden rounded-b-xl
 }
 
 .sidebar ul{
-    @apply z-50 flex items-center justify-center gap-[50%] text-xl font-serif pb-4
+    @apply flex items-center justify-center gap-[35%] text-lg font-serif font-medium pb-4
+}
+
+.sidebar li{
+    @apply border-[1px] border-orange px-3 py-1 rounded-xl
 }
 
 .open{
-    @apply h-40 transition-all duration-500 opacity-40
+    @apply h-40 transition-all duration-500
 }
 
 </style>
