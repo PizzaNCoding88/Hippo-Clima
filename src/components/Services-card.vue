774e1602b07447b71aa97656ad1a1c5d951f68de
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2024-02-06 08:44:02 +0000

    services cards created

--- src/components/Services-card.vue
+++ src/components/Services-card.vue
@@ -1,10 +1,23 @@
 <template>
-    <div class="container-card"></div>
+    <div class="container-card">
+        <!-- {{console.log(src)}} -->
+        <img v-bind:src="src" alt="card-img">
+       <!-- <img src=" '../images' + src" alt=""> -->
+        <h3>{{ title }}</h3>
+        <p>{{ text }}</p>
+    </div>
 </template>
 
 <script>
+
+export default{
+    props: ['src','title', 'text'],
+    
+}
 </script>
 
 <style scoped>
-
+    .container-card{
+        @apply text-black
+    }
 </style>
--- src/components/Services-section.vue
+++ src/components/Services-section.vue
@@ -2,7 +2,11 @@
     <section>
         <div class="container">
             <h2>Scopri i nostri servizi</h2>
-            <ServicesCard />
+            <ul>
+                <li v-for="(card,i) in cardsItems" v-bind:key="i">
+                    <ServicesCard v-bind:src="card.src" :title="card.title" :text="card.text " />
+                </li>
+            </ul>
         </div>
     </section>
 </template>
@@ -11,14 +15,29 @@
 import ServicesCard from './Services-card.vue'
 export default{
   components:{ServicesCard},
-
+  data(){
+    return{
+        cardsItems:[
+            {
+                src: '../../../src/images/boiler.png',
+                title: 'Installazione ed assistenza caldaie',
+                text: 'Installazione nuove caldaie, manutenzione e riparazione di impianti esistenti. Ricorda che la manutenzione annuale delle caldaie é d’obbligo per legge.'
+            },
+            {
+                src: '../../../src/images/aircon.png',
+                title: 'Installazione impianti di condizionamneto',
+                text: 'Grazie alle nostre certificazioni gas, siamo abilitati all’installazione di impianti di condizionamento adatti al vostro ambiente'
+            },
+        ]
+    }
+  }
 }
 
 </script>
 
 <style scoped>
     .container{
-        @apply px-8 bg-white pt-8 text-center
+        @apply px-8 bg-white py-16 text-center
     }
 
     h2{
