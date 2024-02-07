1d4cd6b1d8b0aa3e3cd2d99b320db9b0f26f203f
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2024-02-07 09:02:19 +0000

    form started

--- src/components/Contacts-section.vue
+++ src/components/Contacts-section.vue
@@ -1,9 +1,39 @@
 <template>
-    <section></section>
+    <section class="container">
+        <p>Per un preventivo gratuito o maggiori informazioni, inviaci un messaggio utilizzando il modulo qui sotto, risponderemo al piú presto</p>
+        <Form />
+    </section>
 </template>
 
 <script>
-export default{}
+import Form from './Form-component.vue';
+export default{
+    components:{Form}
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.container{
+    @apply  py-10 px-8 flex flex-col gap-12 text-center relative
+}
+.container{
+    background-image: url('../images/contacts-bg.webp');
+    background-size: cover;    
+}
+
+.container::before{
+    content: "";
+    position: absolute;
+    top:0;
+    bottom:0;
+    left:0;
+    width: 100%;
+    background: black;
+    opacity:0.3;
+}
+
+p{
+    @apply text-2xl font-semibold z-20
+}
+</style>
---
+++ src/components/Form-component.vue
@@ -0,0 +1,52 @@
+<template>
+    <form>
+        <input type="text" name="name" id="name" placeholder="Nome">
+        <label for="name"></label>
+        <input type="text" name="email" id="email" placeholder="email">
+        <label for="email"></label>
+        <textarea name="Messaggio" id="messaggio" cols="30" rows="10" placeholder="Scrivi il tuo messaggio qui..."></textarea>
+        <button type="submit">Invia</button>
+        <span class="decorations first">
+            <span></span>
+        </span>
+        <span class="decorations second"><span></span></span>
+        <span class="decorations third"><span></span></span>
+        <span class="decorations fourth"><span></span></span>
+    </form>
+    
+</template>
+
+<script>
+export default{}
+</script>
+
+<style scoped>
+    form{
+        @apply flex flex-col gap-3 p-8 z-20 bg-[#818181] relative
+    }
+
+    .decorations{
+        @apply w-2 h-2 rounded-full bg-orange absolute
+    }
+
+    .decorations span{
+        @apply w-[0.35rem] h-[0.35rem] rounded-full bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+    }
+
+    .first{
+        @apply top-2 left-2
+    }
+
+    .second{
+        @apply top-2 right-2
+    }
+
+    .third{
+        @apply bottom-2 left-2
+    }
+
+    .fourth{
+        @apply bottom-2 right-2
+    }
+
+</style>
